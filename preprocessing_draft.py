e6bbcd328e62186498ff79275c4eb237960497db
Jost Arndt <jost.arndt@hhi.fraunhofer.de>
2022-07-14 17:18:13 +0200

    Sliding window over a file works. Also every measure gets transformed into 4/4

--- preprocessing_draft.py
+++ preprocessing_draft.py
@@ -76,19 +76,82 @@ def get_node_features(output_tensor, chromatic_scale=['C','C#','D','D#','E','F',
         node_features[feature] = tmp_stack
     return torch.tensor(node_features.values)
 
-def midi_to_vectors(midi_file, segments = 16, delta_t = 0.25):
+def midi_to_vectors(midi_file, segments = 16, delta_t = 0.25): #0.0625 [16x12x1]
     midi_file_chord = midi_file.chordify()
     
+    #rescale this to 4/4: |1 1 1 1| 1 1 1 1|... -> 16 /////// |1/16 1/16 1/16...|
+    midi_44 = midi_file_chord.flatten().getElementsNotOfClass([music21.meter.TimeSignature,music21.layout.LayoutBase]).stream()
+    midi_44.insert(0, music21.meter.TimeSignature('4/4'))
+    midi_44.makeMeasures(inPlace= True)
+    midi_44 = midi_44.makeNotation()
+    #pdb.set_trace()
+
+    #midi_44.show('text') == midi_file_chord.show('text')
+    #TODO check if this is actually true and correct
+    '''pdb.set_trace()
+    for bar in midi_file_chord.measures(2, 3):
+        pdb.set_trace()
+        if type(bar) == music21.stream.base.Measure: 
+            print(bar)
+    pdb.set_trace()
+    '''
+    midi_file_chord = midi_44
+
     total_bars = len(midi_file_chord)
+    #this should work:
+    #midi_file_chord.measure(total_bars).show('text')
+    #this should not work:
+    #midi_file_chord.measure(total_bars+1).show('text')
     
-    bars_in_window = segments * delta_t * 
-    for i in range(total_bars - bars_in_window):
-        for bar in midi_file.chord.measures(i+1, bars_in_window):
-            #check if 4/4-th measure
-            pass
+    bars_in_window = segments * delta_t
+    samples_list = []
+    for i in range(total_bars - int(bars_in_window)):
+        chord_list = []
+        for bar in midi_file_chord.measures(i+1, int(i+bars_in_window)):
+            if type(bar) == music21.stream.base.Measure: 
+                bar_list = []
+                for chord in bar.getElementsByClass('Chord'):
+                    if chord.isNote:
+                        print('ERROR! NOT A CHORD')
+                    elif chord.isChord:
+                        chord_encoded = ([note.midi for note in chord.pitches], chord.offset, chord.offset + chord.duration.quarterLength)
+                        bar_list.append(chord_encoded)
+                bars_in_quarter = extract_quarter(bar_list, amount_quarter = 4)
+                chord_list.append(bars_in_quarter)
+            #return chord_list
+
+        segment_list = []
+        for bar in chord_list:
+            bar_tensor= []
+            for timestep in bar:
+                a = np.array(timestep)%12
+                mlb= MultiLabelBinarizer(classes= list(range(12)))
+                multi_hot = mlb.fit_transform([a]) 
+                signal = [[el] for el in multi_hot[0]]
+                signal_tensor = torch.tensor(signal, dtype = torch.float)
+                #print(signal_tensor)
+                #pdb.set_trace()
+                bar_tensor.append(signal_tensor)
+            segment_list.append(torch.stack(bar_tensor))
+        output_tensor = torch.cat(segment_list) #dim: 16,12,1
 
 
-    chord_list = []
+        # get node features
+        node_features = get_node_features(output_tensor)
+        samples_list.append(node_features)
+    return samples_list
+
+
+
+
+
+    #we want: [samples, 192, 1]
+    #file_two: [samplesize_2, 192,1]a
+    #[sampels + samples_2, 192,1]
+    #so far: [192, 1]
+
+'''
+chord_list = []
     for bar in midi_file_chord.measures(1, 4):#len(midi_file_chord) is max amount of bars
         bar_list = []
         for chord in bar.getElementsByClass('Chord'):
@@ -120,6 +183,8 @@ def midi_to_vectors(midi_file, segments = 16, delta_t = 0.25):
     # get node features
     node_features = get_node_features(output_tensor)
     return node_features
+    return samples_list
+    '''
 
 if __name__ == '__main__':
     token_midi = music21.converter.parse(path)
@@ -135,9 +200,10 @@ if __name__ == '__main__':
     pyg_G = from_networkx(G)
     #print(pyg_G)
 
-    output_tensor = midi_to_vectors(token_midi)
-
-    print(output_tensor.size())
+    output_tensor = midi_to_vectors(token_midi)#ATTENTION: this is a list not a tensor
+    
+    pdb.set_trace()
+    print(output_tensor[0].size())
 
     #pdb.set_trace()
     pyg_G.x = output_tensor
